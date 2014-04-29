424c0fe504984729ea5f69d9424f8a10d1140b60
Paul Golubev <pavel@golubeff.ru>
2014-04-28 20:46:20 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,13 +18,13 @@ set :rails_env, "production"
 set :default_env, { 'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 set :keep_releases, 5
 set :unicorn_config_path, "config/unicorn/production.rb"
-set :unicorn_pid, "tmp/pids/unicorn.pid"
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    invoke 'unicorn:reload'
+    invoke 'unicorn:restart'
   end
 
   after :restart, :clear_cache do
